0b2baae055d231bcf5123f36069ae3304aaba542
parth <138315505+nihilisticneuralnet@users.noreply.github.com>
2025-02-18 08:44:54 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from smolagents import HfApiModel, CodeAgent, ManagedAgent
 from manager import ManimManagerAgent
+from dotenv import load_dotenv
+import os
+from huggingface_hub import login
 
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+login(token=hf_token)
+  
 model = HfApiModel("Qwen/Qwen2.5-72B-Instruct")
 manager = ManimManagerAgent(model)
 
